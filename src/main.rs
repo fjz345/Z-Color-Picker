dfe162729bdbb50f59fa25f00ab66443e37f9666
Filip Zachrisson <fjz345@hotmail.com>
2024-02-02 23:30:26 +0100

    Added todo list

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,28 @@ mod color_picker;
 mod previewer;
 mod ui_common;
 
+/*
+=============================================================
+TODO:
+
+ISSUES
+* remove console from release binary
+* fix preview cell spacing
+
+FEATURES
+* Hex Copy Selected color
+* Hex Copy Gradient color
+* Sample along bezier
+* Different curves
+* Number of points in curve (double click add, right click remove)
+* translate all curve points
+* change hue all curve points
+* Preview colors, change preview cell sizes by sliding
+* Preset save/load
+* beginning/end of curve visuals
+* Preset hue with stepping for points
+=============================================================
+*/
 fn main() {
     env_logger::init(); // Log to stderr (if you run with `RUST_LOG=debug`).
 
