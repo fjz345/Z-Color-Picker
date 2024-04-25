793cd7fde2f4ed6f7634b09f43a8742ccf460add
Filip Zachrisson <fjz345@hotmail.com>
2024-04-25 21:12:02 +0200

    Added quantization for the curved gradient

--- README.md
+++ README.md
@@ -3,54 +3,60 @@
 ![alt text](img/preview_even_newer.png)
 
 # MAIN FEATURES
-* Easy creation of gradients
-* Interactive ui
-* Gradient preview
-* Multiple Spline Modes
-* Create/Save/Load Presets
-* Coded in Rust
+
+- Easy creation of gradients
+- Interactive ui
+- Gradient preview
+- Multiple Spline Modes
+- Create/Save/Load Presets
+- Coded in Rust
 
 # HOW TO
+
 ## Usage
+
 Each control point is a value in a color space (currently only HSV).
 Hue of each individual control point can be changed in the hue bar, and saturation & value in the 2d slider.
 The control-points color is shown in the top left, and underneath displays a gradient using the selected spline mode.
 To create a new preset, click the preset combo box and select NEW
 
 ## Controls
-* Double-click-M1: Add control-point
-* Right-click-M2: Remove control-point
-* Middle-click: Copy color as in the selected format (only on certain widgets for now) 
-* F11/F12: Debug windows
+
+- Double-click-M1: Add control-point
+- Right-click-M2: Remove control-point
+- Middle-click: Copy color as in the selected format (only on certain widgets for now)
+- F11/F12: Debug windows
 
 ## Options
-* Lock: Apply any control point coordinate translation to all control points
-* Auto Hue: Select hue for start/end control points, middle control point's hue is automatically selected
-* Insert Direction: Change the append direction on adding new control point
-* Control Points Constrain: Constrains control-points values to their maximum (this will deform your spline "shape" if any control point touches the edge of any slider) 
+
+- Lock: Apply any control point coordinate translation to all control points
+- Auto Hue: Select hue for start/end control points, middle control point's hue is automatically selected
+- Insert Direction: Change the append direction on adding new control point
+- Control Points Constrain: Constrains control-points values to their maximum (this will deform your spline "shape" if any control point touches the edge of any slider)
 
 # TODO:
 
 ## TODO: Bugs
-* Bezier mode hue interpolation
-* HermiteBezier, can not modify t's in previewer
-* Polynomial
-* Create Preset close button not working
+
+- HermiteBezier, can not modify t's in previewer
+- Polynomial
+- Create Preset close button not working
 
 ## TODO: Features
-* Add confirm window for Delete Preset button
-* Add confirm window for exiting when having an unsaved preset active.
-* Color Lerp
-* Quantizize gradient previewer
-* Regular Color pick for control points
-* Visualization curves
-    - Value
-    - Saturation
-    - Hue
-* User visual feedback when copying color values
-* Middle mouse to copy color from anywhere within the application.
-* Import curves to photoshop????
-* Different color spaces
-* Better ui widget dynamic rezising
-* Preset save more data, lock/etc...
-* Size slider for color picker spline
+
+- Add confirm window for Delete Preset button
+- Add confirm window for exiting when having an unsaved preset active.
+- Color Lerp
+- Quantizize gradient previewer
+- Regular Color pick for control points
+- Visualization curves
+  - Value
+  - Saturation
+  - Hue
+- User visual feedback when copying color values
+- Middle mouse to copy color from anywhere within the application.
+- Import curves to photoshop????
+- Different color spaces
+- Better ui widget dynamic rezising
+- Preset save more data, lock/etc...
+- Size slider for color picker spline
--- src/previewer.rs
+++ src/previewer.rs
@@ -11,11 +11,72 @@ use crate::{
     color_picker::{ColorStringCopy, SplineMode},
     curves::{control_points_to_spline, find_spline_max_t, flatten_control_points},
     gradient::color_function_gradient,
+    hsv_key_value::HsvKeyValue,
     ui_common::{color_button, response_copy_color_on_click},
     ControlPointType,
 };
 
-fn ui_previewer_control_points(
+fn ui_previewer_colors(
+    ui: &mut Ui,
+    size: Vec2,
+    control_points: &[ControlPointType],
+    color_copy_format: ColorStringCopy,
+) -> Response {
+    let rect = Rect::from_min_size(ui.available_rect_before_wrap().min, size);
+    let response = ui.allocate_rect(rect, Sense::click_and_drag());
+    let mut previewer_ui_control_points =
+        ui.child_ui(rect, Layout::left_to_right(egui::Align::Min));
+
+    previewer_ui_control_points.spacing_mut().item_spacing = Vec2::ZERO;
+
+    let ui_size: Vec2 = previewer_ui_control_points.available_size();
+
+    let num_control_points = control_points.len();
+    let size_per_color_x = ui_size.x / (num_control_points as f32);
+    let size_per_color_y = ui_size.y;
+
+    let mut points: Vec<Vec2> = Vec::with_capacity(num_control_points);
+    for cp in control_points {
+        points.push(Vec2::new(cp[0], cp[1]));
+    }
+
+    for i in 0..num_control_points {
+        if points.len() <= i {
+            break;
+        }
+        let color_data = &points[i];
+        let color_data_hue = control_points[i][2];
+        let color_at_point: HsvaGamma = HsvaGamma {
+            h: color_data_hue,
+            s: color_data.x,
+            v: color_data.y,
+            a: 1.0,
+        };
+
+        let size_weight: f32 = 1.0;
+        let response_button: Response = color_button(
+            &mut previewer_ui_control_points,
+            Vec2 {
+                x: size_weight * size_per_color_x,
+                y: size_per_color_y,
+            },
+            color_at_point.into(),
+            true,
+        );
+
+        response_copy_color_on_click(
+            ui,
+            &response_button,
+            color_at_point,
+            color_copy_format,
+            PointerButton::Middle,
+        );
+    }
+
+    response
+}
+
+fn ui_previewer_control_points_with_drag(
     ui: &mut Ui,
     size: Vec2,
     control_points: &[ControlPointType],
@@ -169,6 +230,61 @@ fn ui_previewer_curve(
     });
 }
 
+fn ui_previewer_curve_quantized(
+    ui: &mut Ui,
+    size: Vec2,
+    control_points: &[ControlPointType],
+    spline_mode: SplineMode,
+    previewer_data: &mut PreviewerData,
+    color_copy_format: ColorStringCopy,
+    number_levels: usize,
+) {
+    let flatten_control_points = flatten_control_points(control_points);
+    let mut spline = control_points_to_spline(&flatten_control_points[..], spline_mode);
+
+    match spline_mode {
+        SplineMode::HermiteBezier => {}
+        _ => spline = modify_spline_t_to_preview_sizes(spline, spline_mode, previewer_data),
+    };
+
+    let max_t = find_spline_max_t(&spline);
+
+    let mut quantized_colors: Vec<HsvKeyValue> = Vec::new();
+    for i in 0..number_levels {
+        let sample_x = match spline_mode {
+            SplineMode::HermiteBezier => {
+                1.0 + i as f32 / (number_levels) as f32 as f32 * (max_t - 2.0) as f32
+            }
+            _ => i as f32 / (number_levels - 1) as f32 as f32 * max_t,
+        };
+
+        let sample = spline.clamped_sample(sample_x).unwrap_or_default();
+        quantized_colors.push(sample);
+    }
+
+    ui_previewer_colors(ui, size, &quantized_colors, color_copy_format);
+}
+
+fn ui_previewer_options(ui: &mut Ui, size: Vec2, previewer_data: &mut PreviewerData) {
+    let slider = egui::Slider::new(
+        &mut previewer_data.quantize_num_levels,
+        1..=previewer_data.control_points.len(),
+    );
+    let slider_size: Vec2 = Vec2::new(300.0, 25.0);
+    let mut slider_button_rect: Rect = Rect::from_min_size(
+        egui::Pos2 {
+            x: ui.max_rect().min.x,
+            y: ui.max_rect().min.y,
+        },
+        slider_size,
+    );
+    slider_button_rect = slider_button_rect.translate(Vec2::new(0.0, 25.0));
+
+    if ui.put(slider_button_rect, slider).clicked() {
+        previewer_data.reset_preview_sizes();
+    }
+}
+
 pub fn ui_previewer(
     ui: &mut Ui,
     control_points: &[ControlPointType],
@@ -179,7 +295,7 @@ pub fn ui_previewer(
     let previewer_rect = ui.available_rect_before_wrap();
 
     let inner_response = ui.vertical(|ui| {
-        let control_points_response = ui_previewer_control_points(
+        let control_points_response = ui_previewer_control_points_with_drag(
             ui,
             previewer_rect.size() * Vec2::new(1.0, 0.5),
             control_points,
@@ -188,9 +304,23 @@ pub fn ui_previewer(
         );
         ui_previewer_curve(
             ui,
-            previewer_rect.size() * Vec2::new(1.0, 0.5),
+            previewer_rect.size() * Vec2::new(1.0, 0.25),
             control_points,
             spline_mode,
+            previewer_data,
+        );
+        ui_previewer_curve_quantized(
+            ui,
+            previewer_rect.size() * Vec2::new(1.0, 0.25),
+            control_points,
+            spline_mode,
+            previewer_data,
+            color_copy_format,
+            previewer_data.quantize_num_levels,
+        );
+        ui_previewer_options(
+            ui,
+            previewer_rect.size() * Vec2::new(1.0, 1.0),
             previewer_data,
         );
 
@@ -216,6 +346,7 @@ pub struct PreviewerData {
     pub control_points: Vec<ControlPointType>,
     pub spline_mode: SplineMode,
     pub points_preview_sizes: Vec<f32>,
+    pub quantize_num_levels: usize,
 }
 
 impl PreviewerData {
@@ -224,6 +355,7 @@ impl PreviewerData {
             points_preview_sizes: vec![PREVIEWER_DEFAULT_VALUE; num],
             control_points: vec![ControlPointType::default(); num],
             spline_mode: SplineMode::HermiteBezier,
+            quantize_num_levels: num,
         }
     }
     pub fn reset_preview_sizes(&mut self) {
@@ -264,6 +396,7 @@ impl ZPreviewer {
 
         if old_size != new_size {
             self.data.points_preview_sizes = vec![PREVIEWER_DEFAULT_VALUE; new_size];
+            self.data.quantize_num_levels = new_size;
         }
     }
 
